50ad3540927196b05de2888b0b7d6a661ea0653d
jyfryamyqw-png <jyfryamyqw@gmail.com>
2026-06-19 21:46:07 +0200

    Update bot and pairing numbers in settings

--- function/settings/settings.js
+++ function/settings/settings.js
@@ -14,8 +14,8 @@ global.setting = {
  }
 
 global.info = {
- nomerbot : '212605784394',
- pairingNumber : '212605784394',
+ nomerbot : '584261983267',
+ pairingNumber : '584261983267',
  figlet: 'silanalite', // create a start console display
  nomorwa : '212605784394',
  nameown : 'NOUREDDINE',
